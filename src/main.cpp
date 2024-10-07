f24af1a52f05ffdcb6b6d41757e2884b86462d7b
Lennon-Tijmes <lennon.tijmes@student.nhlstenden.com>
2024-10-07 12:01:34 +0200

    Created_The_Function_menu_which_uses_bools_to_send_you_to_the_correct_function

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,11 @@
 
 bool sum_option = false;
 bool multi_option = false;
+
 // put function declarations here:
 int myFunction(int, int);
+
+int menu(bool sum_option, bool multi_option);
 
 void setup() {
   // put your setup code here, to run once:
@@ -21,6 +24,25 @@ int myFunction(int x, int y) {
 }
 
 int Menu(bool sum_option, bool multi_option) {
-  printf("Type sum for adding something together or multi for multiplication");
-  
+  printf("Type 'sum' for adding something together or 'multi' for multiplication");
+  while (Serial.available() == 0)
+  {
+    // Waiting for a response
+  }
+
+  String input = Serial.readString(); // Reads the input in a string format
+  input.toLowerCase(); // Converts the input into lowercase
+  input.trim(); // Removes the blank spaces in the beginning of the end of the string
+
+  if(String input = "sum")
+  {
+    bool sum_option = true;
+    printf("You have selected the adding function");
+  }
+
+  if(String input = "multi")
+  {
+    bool multi_option = true;
+    printf("You have selected the multiplication function");
+  }
 }
