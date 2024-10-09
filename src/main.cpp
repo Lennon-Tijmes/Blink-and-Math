d5d8c52f108cc18656e56539296a04c0fe2f4ed0
Lennon-Tijmes <lennon.tijmes@student.nhlstenden.com>
2024-10-09 14:35:30 +0200

    Made sure that it correctly redirects to the correct function, added comment lines where function calls can be placed once the others pushed theirs

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,13 @@ bool multi_option = false;
 int menu_option = 0;
 
 // put function declarations here:
-int Menu(bool sum_option, bool multi_option);
+int Menu();
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  int Menu(bool sum_option, bool multi_option);
+  delay(1000);
+  Menu();
 }
 
 void loop() {
@@ -19,27 +20,31 @@ void loop() {
 }
 
 // put function definitions here:
-int Menu(bool sum_option, bool multi_option) {
-  printf("Type '1' for the sum option or '2' for multiplication");
+int Menu() {
+  Serial.println("Type '1' for the sum option or '2' for multiplication");
   while (Serial.available() == 0)
   {
     // Waiting for a response
   }
-  do
-  {
-    int menu_option = Serial.parseInt();
-  }
-  while (menu_option == 1 || menu_option == 2);
+  
+  menu_option = Serial.parseInt();
 
-  if(int menu_option = 1)
+  if(menu_option == 1)
   {
     bool sum_option = true;
-    printf("You have selected the adding function");
+    bool multi_option = false;
+    Serial.println("You have selected the adding function");
+    // add the sum function call here
   }
-
-  if(int menu_option = 2)
+  else if(menu_option == 2)
   {
     bool multi_option = true;
-    printf("You have selected the multiplication function");
+    bool sum_option = false;
+    Serial.println("You have selected the multiplication function");
+    // add the multiplication function call here
+  }
+  else
+  {
+    Serial.println("Please make sure to type in '1' or '2' correctly.");
   }
 }
