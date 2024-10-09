79751e75ce3f0312926e454ac45a013975aa4aa5
Lennon-Tijmes <lennon.tijmes@student.nhlstenden.com>
2024-10-09 09:22:12 +0200

    Changed the way the menu function work from string input to integer input as per the assignment

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,15 @@
 
 bool sum_option = false;
 bool multi_option = false;
+int menu_option = 0;
 
 // put function declarations here:
-int myFunction(int, int);
-
-int menu(bool sum_option, bool multi_option);
+int Menu(bool sum_option, bool multi_option);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  int Menu(bool sum_option, bool multi_option);
 }
 
 void loop() {
@@ -19,28 +19,25 @@ void loop() {
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
-
 int Menu(bool sum_option, bool multi_option) {
-  printf("Type 'sum' for adding something together or 'multi' for multiplication");
+  printf("Type '1' for the sum option or '2' for multiplication");
   while (Serial.available() == 0)
   {
     // Waiting for a response
   }
+  do
+  {
+    int menu_option = Serial.parseInt();
+  }
+  while (menu_option == 1 || menu_option == 2);
 
-  String input = Serial.readString(); // Reads the input in a string format
-  input.toLowerCase(); // Converts the input into lowercase
-  input.trim(); // Removes the blank spaces in the beginning of the end of the string
-
-  if(String input = "sum")
+  if(int menu_option = 1)
   {
     bool sum_option = true;
     printf("You have selected the adding function");
   }
 
-  if(String input = "multi")
+  if(int menu_option = 2)
   {
     bool multi_option = true;
     printf("You have selected the multiplication function");
